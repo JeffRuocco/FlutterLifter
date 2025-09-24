85d47ff11ad91b361cdd3f8885675db2350ded66
Jeff Ruocco <jruocco36@gmail.com>
2025-09-23 21:44:26 -0400

    Refactor meta theme color removal logic for improved clarity

--- lib/utils/web_theme_helper_web.dart
+++ lib/utils/web_theme_helper_web.dart
@@ -12,9 +12,10 @@ class WebThemeHelper {
       // Remove existing theme-color meta tags
       final existingTags =
           web.document.querySelectorAll('meta[name="theme-color"]');
-      existingTags.toList().forEach((element) {
-        (element as web.Element?)?.remove();
-      });
+      for (int i = 0; i < existingTags.length; i++) {
+        final element = existingTags.item(i) as web.Element?;
+        element?.remove();
+      }
 
       // Create and add new theme-color meta tag
       final metaTag = web.document.createElement('meta') as web.HTMLMetaElement;
